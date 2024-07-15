9668e7e413f8760f86f0e9fc848bf1f6723ffdc1
yousakamori <yousakamori@gmail.com>
2024-07-15 10:17:42 +0900

    added server alias settings

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import { hc } from "hono/client";
-import { type ApiRoutes } from "../../../server/app";
+import { type ApiRoutes } from "@server/app";
 const client = hc<ApiRoutes>("/");
 
 export const api = client.api;
