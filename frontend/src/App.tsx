3faada5edee29d8e318d11986baed46d9924bf80
yousakamori <yousakamori@gmail.com>
2024-07-15 10:12:58 +0900

    added hono rpc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useEffect, useState } from "react";
+import { api } from "@/lib/api";
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
 
   useEffect(() => {
     async function fetchTotal() {
-      const res = await fetch("/api/expenses/total-spent");
+      const res = await api.expenses["total-spent"].$get();
       const data = await res.json();
       setTotalSpent(data.total);
     }
