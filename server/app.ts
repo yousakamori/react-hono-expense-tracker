5de6b6b42f502bd089a7c89300a633b378a6119d
yousakamori <yousakamori@gmail.com>
2024-07-15 10:12:17 +0900

    added server static setting

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expesesRoute";
+import { serveStatic } from "hono/bun";
 const app = new Hono();
 
 app.use("*", logger());
-app.get("/test", (c) => c.json({ message: "Hono!" }));
-app.route("/api/expenses", expensesRoute);
+
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
+export type ApiRoutes = typeof apiRoutes;
