62584a21e553a570baa647adb8f59c5889096142
yousakamori <yousakamori@gmail.com>
2024-07-20 19:48:21 +0900

    changed alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dir, "./src"),
-      "@server": path.resolve(import.meta.dir, "../server"),
+      "@": path.resolve(__dirname, "./src"),
+      "@server": path.resolve(__dirname, "../server"),
     },
   },
   server: {
