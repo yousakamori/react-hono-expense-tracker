dba6b7d43874f44b6d2f80987b94dd4d5631fb22
yousakamori <yousakamori@gmail.com>
2024-07-20 19:48:31 +0900

    changed profile

--- frontend/src/routes/_authenticated/profile.tsx
+++ frontend/src/routes/_authenticated/profile.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { userQueryOptions } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authenticated/profile")({
   component: Profile,
@@ -19,9 +21,20 @@ function Profile() {
 
   return (
     <div className="p-2">
-      Hello from About!
-      <p>{data.user.family_name}</p>
-      <a href="/api/logout">Logout!</a>
+      <div className="flex items-center gap-2">
+        <Avatar>
+          {data.user.picture && (
+            <AvatarImage src={data.user.picture} alt={data.user.given_name} />
+          )}
+          <AvatarFallback>{data.user.given_name}</AvatarFallback>
+        </Avatar>
+        <p>
+          {data.user.given_name} {data.user.family_name}
+        </p>
+      </div>
+      <Button asChild className="my-4">
+        <a href="/api/logout">Logout!</a>
+      </Button>
     </div>
   );
 }
