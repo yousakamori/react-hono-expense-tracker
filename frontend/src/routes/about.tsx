aa9ae9d21eebd7d496e888ba9bb042bbe0df7ed5
yousakamori <yousakamori@gmail.com>
2024-07-20 19:28:13 +0900

    changed about

--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -5,5 +5,5 @@ export const Route = createFileRoute("/about")({
 });
 
 function About() {
-  return <div className="p-2">Hello from About!</div>;
+  return <div className="p-2">Coming soon!</div>;
 }
